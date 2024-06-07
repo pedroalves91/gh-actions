3e1d7e8eb043dfad59b79c8cddcca363f0023139
pedro <pedro.alves@thetakenseat.com>
2024-06-07 22:02:26 +0100

    workflow 17.0.5

---
+++ .github/actions/docker-ping-url/main.py
@@ -0,0 +1,12 @@
+if __name__ == "__main__":
+    print("Hello, World!")
+    #import requests
+    #import sys
+
+    #url = sys.argv[1]
+    #response = requests.get(url)
+    #if response.status_code == 200:
+        #print(f"Successfully pinged {url}")
+    #else:
+        #print(f"Failed to ping {url}")
+        #sys.exit(1)
