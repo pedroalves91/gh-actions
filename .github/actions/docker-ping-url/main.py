f78d67614f5922a559ee73df267ec7f0cd2f1fde
pedro <pedro.alves@thetakenseat.com>
2024-06-07 22:27:20 +0100

    workflow 17.0.7

--- .github/actions/docker-ping-url/main.py
+++ .github/actions/docker-ping-url/main.py
@@ -1,12 +1,36 @@
-if __name__ == "__main__":
-    print("Hello, World!")
-    #import requests
-    #import sys
+import os
+import requests
+from time import sleep
 
-    #url = sys.argv[1]
-    #response = requests.get(url)
-    #if response.status_code == 200:
-        #print(f"Successfully pinged {url}")
-    #else:
-        #print(f"Failed to ping {url}")
-        #sys.exit(1)
+def ping_url(website_url, delay, max_trials):
+    trials = 0
+    while trials < max_trials:
+        try:
+            response = requests.get(website_url)
+            if response.status_code == 200:
+                print(f"Successfully pinged {website_url}")
+                return True
+        except requests.ConnectionError:
+            print(f"Failed to ping {website_url}. Retrying in {delay} seconds...")
+            sleep(delay)
+            trials += 1
+        except requests.exceptions.MissingSchema:
+            print(f"Invalid URL: {website_url}")
+            return False
+    return False
+
+def run():
+    print("Hello, World!")
+    website_url = os.getenv("INPUT_URL")
+    delay = int(os.getenv("INPUT_DELAY"))
+    max_trials = int(os.getenv("INPUT_MAX_TRIALS"))
+
+    website_reachable = ping_url(website_url, delay, max_trials)
+
+    if not website_reachable:
+        raise Exception(f"Failed to ping {website_url} after {max_trials} trials")
+
+    print(f"Successfully pinged the website! {website_url}")
+
+if __name__ == "__main__":
+    run()
